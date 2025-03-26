f202f030f6cc1704d1f752616959e61d003d362e
AlienegraGeek <969024145@qq.com>
2025-03-27 00:14:42 +0800

    docs(swagger): docs

--- model/user.go
+++ model/user.go
@@ -15,22 +15,6 @@ type User struct {
 	Flag      string    // 启用标志(1-启用 0-停用)
 }
 
-// UserRefreshManagerToken
-//
-//	@Description:	修改指定用户的token数据
-//	@param			token	数据格式	<token_value:timestamp>
-//	@return			err
-func UserRefreshManagerToken(db *gorm.DB, userId int64, token string) (err error) {
-	res := db.Model(&User{}).Where("id = ?", userId).Update("manager_token", token)
-	if res.Error != nil {
-		return res.Error
-	}
-	if res.RowsAffected == 0 {
-		return errors.New("res.RowsAffected == 0")
-	}
-	return nil
-}
-
 // UserSelectIdByToken token查询用户数据 token = "HASH"
 func UserSelectIdByToken(db *gorm.DB, token string) (user User, err error) {
 	err = db.Table("users").
@@ -41,7 +25,7 @@ func UserSelectIdByToken(db *gorm.DB, token string) (user User, err error) {
 // UserRefreshToken
 //
 //	@Description:	修改指定用户的token数据
-//	@param			token	数据格式	<token_value:timestamp>
+//	token	数据格式	<token_value:timestamp>
 //	@return			err
 func UserRefreshToken(db *gorm.DB, userId int64, token string) (err error) {
 	res := db.Model(&User{}).Where("id = ?", userId).Update("token", token)
